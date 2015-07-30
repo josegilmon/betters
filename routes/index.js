2b5c2ef5e323cb6c6fd607045ec18db1d51aedc0
Jose Antonio Gil <joseantonio.gil@intelygenz.com>
2015-07-30 15:09:26 +0200

    Prueba Jade & CSS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/list', function(req, res, next) {
+	res.render('list');
 });
 
 module.exports = router;
