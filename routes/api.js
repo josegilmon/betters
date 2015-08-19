a99ad4f4ec3301d51aa785d8cf71375ab30302cf
Jose Antonio Gil <joseantonio.gil@intelygenz.com>
2015-08-19 15:14:12 +0200

    Cache y api controller

--- routes/api.js
+++ routes/api.js
@@ -2,31 +2,44 @@ var cache = require('../lib/cache');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', error404);
-
 /* GET  */
-router.get('/fixtures', function(req, res, next) {
-	cache.loadCache('http://api.football-data.org/alpha/soccerseasons/399/fixtures');
-});
+//router.get('/', controller.error);
+router.get('/fixtures', loadFixtures);
+router.get('/fixtures/:matchday', loadFixture);
 
-/* GET  */
-router.get('/fixtures/:matchday', function(req, res, next) {
+router.get('/teams', loadTeams);
 
-	var matchday = req.params.matchday || 1;
+// catch 404 and forward to error handler
+router.get('*', failed);
 
-	cache.loadCache('http://api.football-data.org/alpha/soccerseasons/399/fixtures?matchday=' + (matchday || 1), function (data) {
+
+var baseUrl = 'http://api.football-data.org/alpha/soccerseasons/399';
+
+function failed (req, res, next) {
+	res.status(404).send('Not found');
+}
+function loadFixtures(req, res, next) {
+	cache.loadCache(baseUrl + '/fixtures', function (data) {
 		res.set('Content-Type', 'application/json')
 			.status(200)
 			.send(data);
-	})
-});
-
-router.get('/list', function(req, res, next) {
-	res.render('list');
-});
-
-function error404(req, res, next) {
-	res.status(404).send('Not found');
+	});
 }
+function loadFixture(req, res, next) {
+	var matchday = req.params.matchday || 1;
+	cache.loadCache(baseUrl + '/fixtures?matchday=' + (matchday || 1), function (data) {
+		res.set('Content-Type', 'application/json')
+			.status(200)
+			.send(data);
+	});
+}
+function loadTeams(req, res, next) {
+	cache.loadCache(baseUrl + '/teams', function (data) {
+		res.set('Content-Type', 'application/json')
+			.status(200)
+			.send(data);
+	});
+}
+
 
 module.exports = router;
