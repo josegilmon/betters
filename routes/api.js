96ba7a03a76aef55215b6ba5076dceeae86a88db
Jose Antonio Gil <joseantonio.gil@intelygenz.com>
2015-08-22 21:24:09 +0200

    Refactorización Cache

--- routes/api.js
+++ routes/api.js
@@ -14,30 +14,37 @@ router.get('*', failed);
 
 
 var baseUrl = 'http://api.football-data.org/alpha/soccerseasons/399';
+var dataFilePath = '../public/data';
 
-function failed (req, res, next) {
+function success(res, data) {
+	res.set('Content-Type', 'application/json')
+		.status(200)
+		.send(data);
+}
+function failed(req, res, next) {
 	res.status(404).send('Not found');
 }
 function loadFixtures(req, res, next) {
-	cache.loadCache(baseUrl + '/fixtures', function (data) {
-		res.set('Content-Type', 'application/json')
-			.status(200)
-			.send(data);
+	cache.loadCache(baseUrl + '/fixtures', {
+		success: function (data) {
+			success(res, data);
+		}
 	});
 }
 function loadFixture(req, res, next) {
 	var matchday = req.params.matchday || 1;
-	cache.loadCache(baseUrl + '/fixtures?matchday=' + (matchday || 1), function (data) {
-		res.set('Content-Type', 'application/json')
-			.status(200)
-			.send(data);
+	cache.loadCache(baseUrl + '/fixtures?matchday=' + (matchday || 1), {
+		success: function (data) {
+			success(res, data);
+		}
 	});
 }
 function loadTeams(req, res, next) {
-	cache.loadCache(baseUrl + '/teams', function (data) {
-		res.set('Content-Type', 'application/json')
-			.status(200)
-			.send(data);
+	cache.loadCache(baseUrl + '/teams', {
+		saveAs: dataFilePath + '/teams.json',
+		success: function (data) {
+			success(res, data);
+		}
 	});
 }
 
